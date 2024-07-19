b91d58daa0fbd375e86a169e1966a39427992a7e
thdus1323 <thdus1323@naver.com>
2024-07-19 17:14:05 +0900

    오타수ì 정

--- src/main/java/site/metacoding/blogv3/category/CategoryService.java
+++ src/main/java/site/metacoding/blogv3/category/CategoryService.java
@@ -14,8 +14,8 @@ public class CategoryService {
     //카테고리 등록
     @Transactional
     public void save(Category category){  // isPresent 써서 있으면 못 쓰게하고,  없으면 사용할 수 있도록!
-        Optional<Category> category1 = categoryRepository.findCategoriesBy(category.getUser(), category.getCategoryName());
-        if (category1.isPresent()){
+        Optional<Category> categoryIsExists = categoryRepository.findCategoriesBy(category.getUser(), category.getCategoryName());
+        if (categoryIsExists.isPresent()){
             throw new IllegalStateException("카테고리가 중복되었어요");
         }else {
             categoryRepository.save(category);
